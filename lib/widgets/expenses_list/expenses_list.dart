d605dba860f65f5ab7c6785e4e5152a2db0fc79c
AnesuMurakata <murac96@gmail.com>
2024-12-17 16:19:33 +0200

    add theme styles

--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -18,6 +18,11 @@ class ExpensesList extends StatelessWidget {
       itemCount: expenses.length,
       itemBuilder: (context, index) => Dismissible(
         key: ValueKey(expenses[index]),
+        background: Container(
+          color: Theme.of(context).colorScheme.error.withValues(alpha: 0.6),
+          //margin: EdgeInsets.symmetric(
+          //  vertical: Theme.of(context).cardTheme.margin!.vertical),
+        ),
         onDismissed: (direction) {
           onRemoveExpense(expenses[index]);
         },
